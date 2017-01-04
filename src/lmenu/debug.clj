1848f68c8c2d20611784d160cf576ba5694558d4
MinasMazar <minasmazar@gmail.com>
2017-01-04 01:27:59 +0100

    Add stolen-gist about debugging Clojure breakpointing the code

---
+++ src/lmenu/debug.clj
@@ -0,0 +1,19 @@
+(ns lmenu.debug)
+
+(defn contextual-eval [ctx expr]
+  (eval
+   `(let [~@(mapcat (fn [[k v]] [k `'~v]) ctx)]
+      ~expr)))
+(defmacro local-context []
+  (let [symbols (keys &env)]
+    (zipmap (map (fn [sym] `(quote ~sym)) symbols) symbols)))
+(defn readr [prompt exit-code]
+  (let [input (clojure.main/repl-read prompt exit-code)]
+    (if (= input ::tl)
+      exit-code
+      input)))
+(defmacro break []
+  `(clojure.main/repl
+    :prompt #(print "debug=> ")
+    :read readr
+    :eval (partial contextual-eval (local-context))))
